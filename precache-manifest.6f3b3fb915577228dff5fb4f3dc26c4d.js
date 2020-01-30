0bb67f823a160d4f597dffe67f119c58e4ba143b
Vikrant <vikrantbhadauriya2@gmail.com>
2020-01-31 00:17:32 +0530

    final

---
+++ precache-manifest.6f3b3fb915577228dff5fb4f3dc26c4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83021f7a7b6059cbbc6502d270f1d940",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4499ecf2a7352f972820",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7664b4ffec9587d672f9",
+    "url": "/static/css/main.057f5fe0.chunk.css"
+  },
+  {
+    "revision": "4499ecf2a7352f972820",
+    "url": "/static/js/2.d472115f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d472115f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7664b4ffec9587d672f9",
+    "url": "/static/js/main.67b85ff5.chunk.js"
+  },
+  {
+    "revision": "b7ee122ad1e80e2b2540",
+    "url": "/static/js/runtime-main.adc99469.js"
+  }
+]);
